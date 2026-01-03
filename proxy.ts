ebdcba07826a59ed9528e81b2d3553321702130d
Maksim <70702008m@gmail.com>
2026-01-03 22:57:13 +0200

    text

--- proxy.ts
+++ proxy.ts
@@ -5,29 +5,37 @@ type ProxyRequest = Request & {
   };
 };
 
-const PRIVATE_ROUTES = ['/profile', '/notes', '/dashboard'];
-const PUBLIC_ROUTES = ['/login', '/register'];
+
+const PRIVATE_ROUTES = ['/profile', '/notes'];
+
+
+const AUTH_ROUTES = ['/sign-in', '/sign-up'];
 
 export default function proxy(request: ProxyRequest) {
   const { pathname, origin } = request.nextUrl;
 
+  
   const token = request.cookies.get('token')?.value;
 
+  
   const isPrivateRoute = PRIVATE_ROUTES.some(route =>
     pathname.startsWith(route)
   );
 
-  const isPublicRoute = PUBLIC_ROUTES.some(route =>
+  const isAuthRoute = AUTH_ROUTES.some(route =>
     pathname.startsWith(route)
   );
 
-   if (isPrivateRoute && !token) {
-    return Response.redirect(new URL('/login', origin));
+  
+  if (isPrivateRoute && !token) {
+    return Response.redirect(new URL('/sign-in', origin));
   }
 
-    if (isPublicRoute && token) {
+  
+  if (isAuthRoute && token) {
     return Response.redirect(new URL('/profile', origin));
   }
 
-    return new Response(null, { status: 200 });
+  
+  return new Response(null, { status: 200 });
 }
